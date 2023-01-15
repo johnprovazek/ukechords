9d48f12bd63ca7204bff8fe6c6ffea55de7a9f11
johnprovazek <johnprovazek@gmail.com>
2023-01-14 20:52:56 -0800

    finished tutorial on google auth

---
+++ src/pages/gatsby-ssr.js
@@ -0,0 +1,7 @@
+// const React = require("react")
+
+// export const onRenderBody = ({ setHeadComponents }) => {
+//     setHeadComponents([
+//         <script key="blah" src="https://accounts.google.com/gsi/client" async defer></script>
+//     ])
+//   }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,59 @@
 // Step 1: Import React
 import * as React from 'react'
-// import { Link } from 'gatsby'
 import Layout from '../components/layout'
-import Chord from '@tombatossals/react-chords/lib/Chord'
-
-const MyChord = () => {
-  const chord = {
-    "frets": [3,3,3,3],
-    "fingers": [1,1,1,1],
-    "barres": [3],
-    "capo": true
-  }
-  const instrument = {
-      "strings": 4,
-      "fretsOnChord": 4,
-      "name": "ukulele",
-      "keys": [],
-      "tunings": {
-          standard: ["G", "C", "E", "A"]
-      }
-  }
-  const lite = false // defaults to false if omitted
-  return (
-      <Chord
-          chord={chord}
-          instrument={instrument}
-          lite={lite}
-      />
-  )
-}
+import { useEffect, useState } from 'react'
+import jwt_decode from "jwt-decode"
 
 // Step 2: Define your component
 const IndexPage = () => {
+
+  const [user, setUser] = useState({});
+
+  function handleCallbackResponse(response) {
+    console.log(response.credential)
+    var userObject = jwt_decode(response.credential)
+    console.log(userObject)
+    setUser(userObject)
+    document.getElementById("signInDiv").hidden = true; 
+  }
+
+  function handleSignOut(event) {
+    setUser({})
+    document.getElementById("signInDiv").hidden = false; 
+  }
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: "882648076498-mebve0aqktvvd180s5udffucile3g1g3.apps.googleusercontent.com",
+      callback: handleCallbackResponse
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      { theme: "outline", size: "large"}
+    );
+
+    google.accounts.id.prompt();
+
+  }, []);
+
   return (
-    <Layout pageTitle="Uke Chords">
-      <p>This is a website developed to help you learn and memorize ukulele chords. Good luck and have fun!</p>
-      <MyChord />
-    </Layout>
+    <div>
+      <Layout pageTitle="Uke Chords">
+        <p>This is a website developed to help you learn and memorize ukulele chords. Good luck and have fun!</p>
+        { Object.keys(user).length !== 0 &&
+          <button onClick={ (e) => handleSignOut(e)}>Sign Out</button>
+        }
+        <div id="signInDiv"></div>
+        { user && 
+          <div>
+            <img src={user.picture} alt=""></img>
+            <h3>{user.name}</h3>
+          </div>
+        }
+      </Layout>
+    </div>
   )
 }
 
